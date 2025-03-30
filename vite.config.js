4af437e1d266cc9a72271d12c48ebe05403affe8
Matthew Sporman <mfsporma@mtu.edu>
2025-03-30 19:36:57 -0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/music_player",
+  base: "/music_player/",
   plugins: [
     vue(),
     vueDevTools(),
